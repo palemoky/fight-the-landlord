9b99c6143373fefde54cdd19041eca7b8dbb0de7
Palemoky <palemoky@gmail.com>
2026-01-04 17:04:34 +0800

    refactor: Introduce `newGameError` helper and update predefined errors to use it for message consistency.

--- internal/apperrors/errors.go
+++ internal/apperrors/errors.go
@@ -14,15 +14,24 @@ func (e *GameError) Error() string {
 	return e.Message
 }
 
+// newGameError 创建游戏错误，从 protocol.ErrorMessages 获取消息
+func newGameError(code int) *GameError {
+	message := protocol.ErrorMessages[code]
+	if message == "" {
+		message = protocol.ErrorMessages[protocol.ErrCodeUnknown]
+	}
+	return &GameError{Code: code, Message: message}
+}
+
 // 预定义错误
 var (
-	ErrRoomNotFound = &GameError{Code: protocol.ErrCodeRoomNotFound, Message: "房间不存在"}
-	ErrRoomFull     = &GameError{Code: protocol.ErrCodeRoomFull, Message: "房间已满"}
-	ErrNotInRoom    = &GameError{Code: protocol.ErrCodeNotInRoom, Message: "您不在房间中"}
-	ErrGameStarted  = &GameError{Code: protocol.ErrCodeGameNotStart, Message: "游戏已开始"}
-	ErrGameNotStart = &GameError{Code: protocol.ErrCodeGameNotStart, Message: "游戏尚未开始"}
-	ErrNotYourTurn  = &GameError{Code: protocol.ErrCodeNotYourTurn, Message: "还没轮到您"}
-	ErrInvalidCards = &GameError{Code: protocol.ErrCodeInvalidCards, Message: "无效的牌型"}
-	ErrCannotBeat   = &GameError{Code: protocol.ErrCodeCannotBeat, Message: "您的牌大不过上家"}
-	ErrMustPlay     = &GameError{Code: protocol.ErrCodeMustPlay, Message: "您必须出牌"}
+	ErrRoomNotFound = newGameError(protocol.ErrCodeRoomNotFound)
+	ErrRoomFull     = newGameError(protocol.ErrCodeRoomFull)
+	ErrNotInRoom    = newGameError(protocol.ErrCodeNotInRoom)
+	ErrGameStarted  = newGameError(protocol.ErrCodeGameStarted)
+	ErrGameNotStart = newGameError(protocol.ErrCodeGameNotStart)
+	ErrNotYourTurn  = newGameError(protocol.ErrCodeNotYourTurn)
+	ErrInvalidCards = newGameError(protocol.ErrCodeInvalidCards)
+	ErrCannotBeat   = newGameError(protocol.ErrCodeCannotBeat)
+	ErrMustPlay     = newGameError(protocol.ErrCodeMustPlay)
 )
